b4fc0c23b87f67ca46c5344591627e9e57479af8
lasersral34 <lasersral34@gmail.com>
2017-03-31 21:12:42 +0200

    FctResponse implementiert

---
+++ FctResponse.java
@@ -0,0 +1,15 @@
+
+/**
+ * Enumeration class FctResponse - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum FctResponse
+{
+    OK,
+    ERROR,
+    
+    LOGOUT,
+    SHUTDOWN;
+}
